eecc04de34175e3eeb99439a0b667933173ca836
fav757 <fav755@gmail.com>
2020-08-16 22:27:03 +0300

    add fonts awesome cdn

--- src/components/InputPanel/Input.js
+++ src/components/InputPanel/Input.js
@@ -3,8 +3,15 @@ import styled from 'styled-components';
 import { GlobalState } from '../../GlobalState';
 import { changeInputQuery } from '../../RootActions';
 
+const StyledContainer = styled.div`
+  display: flex;
+  position: relative;
+  align-items: center;
+  padding-bottom: 2rem;
+`;
+
 const StyledInput = styled.input`
-  margin-bottom: 2rem;
+  padding-bottom: 2rem;
   background: #23232d;
   border-radius: 24px;
   font-size: 1rem;
@@ -15,17 +22,45 @@ const StyledInput = styled.input`
   outline: none;
   color: inherit;
 
+  :focus {
+    box-shadow: 0 0 5px #d61f6c;
+  }
+
   ::placeholder {
     color: #c8c8c8;
   }
 `;
 
+const StyledIcon = styled.div`
+  position: absolute;
+  right: 0;
+  margin-right: 1rem;
+
+  :hover {
+    transform: scale(1.2);
+  }
+`;
+
 function Input() {
-  const {state, dispatch} = useContext(GlobalState);
+  const { state, dispatch } = useContext(GlobalState);
+
   const handleChange = (event) =>
     dispatch(changeInputQuery(event.target.value));
 
-  return <StyledInput onChange={handleChange} defaultValue={state.inputQuery} placeholder='products 10$' />;
+  const handleClick = () => dispatch(changeInputQuery(''));
+
+  return (
+    <StyledContainer>
+      <StyledIcon onClick={handleClick}>
+        <i className='fas fa-times'></i>
+      </StyledIcon>
+      <StyledInput
+        value={state.inputQuery || ''}
+        onChange={handleChange}
+        placeholder='products 10$'
+      />
+    </StyledContainer>
+  );
 }
 
 export default Input;
