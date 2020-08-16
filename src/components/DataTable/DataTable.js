f796144863a4e5d5b886048326733e156c704743
fav757 <fav755@gmail.com>
2020-08-16 13:45:06 +0300

    add styles for data table component

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledTable = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  text-align: left;
+
+  tr {
+    border-bottom: 1px solid #c8c8c8;
+  }
+
+  tbody tr:last-child {
+    border-bottom: none;
+  }
+
+  td {
+    padding: 0.5rem 1rem;
+    width: 50%;
+  }
+
+  td:first-child {
+    border-right: 1px solid #c8c8c8;
+  }
+`;
+
 function DataTable() {
   return (
-    <table>
+    <StyledTable>
       <thead>
         <tr>
-          <th>Category:</th>
-          <th>Value:</th>
+          <td>Category:</td>
+          <td>Value:</td>
         </tr>
       </thead>
       <tbody>
@@ -20,7 +43,7 @@ function DataTable() {
           <td>10$</td>
         </tr>
       </tbody>
-    </table>
+    </StyledTable>
   );
 }
 
