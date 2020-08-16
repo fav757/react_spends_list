b4f0855921e043a763b4ff040320307ee8e3474d
fav757 <fav755@gmail.com>
2020-08-16 13:23:58 +0300

    add styles for input panel component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import InputPanel from './components/InputPanel/InputPanel';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  >h1 {
+    text-align: center;
+  }
+
+  >*:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`;
 
 function App() {
   return (
-    <React.Fragment>
+    <StyledApp>
       <h1>React spends list</h1>
       <InputPanel />
-    </React.Fragment>
+    </StyledApp>
   );
 }
 
--- src/components/InputPanel/InputPanel.js
+++ src/components/InputPanel/InputPanel.js
@@ -2,18 +2,38 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledInput = styled.input`
+  margin-bottom: 1rem;
   background: #23232d;
-  border-radius: 64px;
+  border-radius: 24px;
   font-size: 1rem;
   text-align: center;
   width: 100%;
   padding: 0.5rem 1rem;
   border: none;
   outline: none;
-  color: #E5E5E5;
+  color: inherit;
 
   ::placeholder {
-    color: #C8C8C8;
+    color: #c8c8c8;
+  }
+`;
+
+const StyledButtonsRow = styled.div`
+  display: flex;
+`;
+
+const StyledButton = styled.button`
+  flex: 1;
+  color: inherit;
+  padding: 0.5rem 1rem;
+  border-radius: 24px;
+  background: #97124a;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border: none;
+  outline: none;
+
+  :first-child {
+    margin-right: 1rem;
   }
 `;
 
@@ -21,10 +41,10 @@ function InputPanel() {
   return (
     <div>
       <StyledInput placeholder='products 10$' />
-      <div>
-        <button>Spends</button>
-        <button>Incomes</button>
-      </div>
+      <StyledButtonsRow>
+        <StyledButton>Spends</StyledButton>
+        <StyledButton>Incomes</StyledButton>
+      </StyledButtonsRow>
     </div>
   );
 }
